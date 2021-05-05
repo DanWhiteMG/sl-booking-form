a5405106f5a6f8c15ba2357d7d2142ac5bca23b9
LeeSquires <leesquires@me.com>
2021-05-05 11:05:50 +0100

    Changed const ENERGY_COST to £199.00

--- booking-form.js
+++ booking-form.js
@@ -7,7 +7,7 @@ document.getElementById('annual-cost').readOnly = true;
 
 // Add energy charge field and populate with cost
 
-const ENERGY_CHARGE = "£99.00";
+const ENERGY_CHARGE = "£199.00";
 
 document.getElementById('energy-charge').readOnly = true;
 document.getElementById('energy-charge').value = ENERGY_CHARGE;
